d24c51b673ddea4df584863e21ce35a7ffd010cb
greenpizza1203 <konakarthik12@gmail.com>
2020-11-03 13:35:53 -0500

    changes

---
+++ src/test_client.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var server_1 = require("./server");
+server_1.be_server(42069);
+//# sourceMappingURL=test_client.js.map
